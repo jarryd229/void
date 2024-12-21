0fe1f5ff6b635526b5298d977103ab71dd8cc478
jarryd229 <15334102+jarryd229@users.noreply.github.com>
2024-12-21 15:55:24 +1100

    added player.teleport(Tile(1234, 1234), "animation")

--- game/src/main/kotlin/content/skill/magic/jewellery/JewelleryTeleport.kt
+++ game/src/main/kotlin/content/skill/magic/jewellery/JewelleryTeleport.kt
@@ -8,14 +8,31 @@ import world.gregs.voidps.engine.map.collision.random
 import world.gregs.voidps.engine.queue.ActionPriority
 import world.gregs.voidps.engine.queue.queue
 import world.gregs.voidps.type.Area
+import world.gregs.voidps.type.Tile
 import content.entity.sound.playSound
 
 fun jewelleryTeleport(player: Player, inventory: String, slot: Int, area: Area): Boolean {
     return itemTeleport(player, inventory, slot, area, "jewellery")
 }
 
+fun Player.teleport(tile: Tile, type: String, force: Boolean = false) = itemTeleport(this, tile, type, force)
+
 fun itemTeleport(player: Player, inventory: String, slot: Int, area: Area, type: String): Boolean {
     if (player.queue.contains(ActionPriority.Normal) || !player.inventories.inventory(inventory).discharge(player, slot)) {
+        return false
+    }
+    return itemTeleport(player, area, type)
+}
+
+fun itemTeleport(player: Player, area: Area, type: String, force: Boolean = false): Boolean {
+    if (!force && player.queue.contains(ActionPriority.Normal)) {
+        return false
+    }
+    return itemTeleport(player, area.random(player) ?: return false, type, force)
+}
+
+fun itemTeleport(player: Player, tile: Tile, type: String, force: Boolean = false): Boolean {
+    if (!force && player.queue.contains(ActionPriority.Normal)) {
         return false
     }
     player.closeInterfaces()
